931015004868b511e3417ddd0a0808f7f0910c61
spinu.vitalina <spinuvitalina555@gmail.com>
2024-07-17 12:47:00 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Dmitrii Vrabie on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
